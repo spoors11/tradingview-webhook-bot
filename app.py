fd9c1684f3a438f14db291e2b3c65a8499e5f952
spoors11 <spoors_11@hotmail.com>
2025-11-04 20:54:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -25,4 +26,5 @@ def home():
     return "TradingView Webhook Bot is running."
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
